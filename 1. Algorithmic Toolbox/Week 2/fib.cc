fa7e34392dc07728867f085379cfd808ccd3862f
aadimator <aadimator@gmail.com>
2016-06-19 12:45:45 +0500

    Week 2 : Completed first problem fib.cc

--- 1. Algorithmic Toolbox/Week 2/fib.cc
+++ 1. Algorithmic Toolbox/Week 2/fib.cc
@@ -7,10 +7,25 @@ int calc_fib(int n) {
     return calc_fib(n - 1) + calc_fib(n - 2);
 }
 
+
+long long calc_fib_faster (int n) {
+    long long arr [n + 1];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return arr[n];
+}
+
+
 int main() {
     int n = 0;
     std::cin >> n;
 
-    std::cout << calc_fib(n) << '\n';
+//    std::cout << calc_fib(n) << std::endl;
+    std::cout << calc_fib_faster(n) << std::endl;
     return 0;
 }
