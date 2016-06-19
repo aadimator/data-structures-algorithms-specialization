8fb88c37602b3acce68e18a57bae6e60b2f7b745
aadimator <aadimator@gmail.com>
2016-06-19 13:25:06 +0500

    Week 2 : Completed third problem gcd.cc

--- 1. Algorithmic Toolbox/Week 2/gcd.cpp
+++ 1. Algorithmic Toolbox/Week 2/gcd.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 
 int gcd(int a, int b) {
-  //write your code here
-  int current_gcd = 1;
-  for (int d = 2; d <= a && d <= b; d++) {
-    if (a % d == 0 && b % d == 0) {
-      if (d > current_gcd) {
-        current_gcd = d;
-      }
+    int current_gcd = 1;
+    for (int d = 2; d <= a && d <= b; d++) {
+        if (a % d == 0 && b % d == 0) {
+            if (d > current_gcd) {
+                current_gcd = d;
+            }
+        }
     }
-  }
-  return current_gcd;
+    return current_gcd;
+}
+
+int euclidGCD (int a, int b) {
+    if (b == 0) return a;
+    return euclidGCD(b, a%b);
 }
 
 int main() {
-  int a, b;
-  std::cin >> a >> b;
-  std::cout << gcd(a, b) << std::endl;
-  return 0;
+    int a, b;
+    std::cin >> a >> b;
+//    std::cout << gcd(a, b) << std::endl;
+    std::cout << euclidGCD(a, b) << std::endl;
+    return 0;
 }
