837a04504d94ed4113502eab14ff6590efb2ea04
aadimator <aadimator@gmail.com>
2016-06-19 13:14:45 +0500

    Week 2 : Completed second problem

--- 1. Algorithmic Toolbox/Week 2/fibonacci_last_digit.cpp
+++ 1. Algorithmic Toolbox/Week 2/fibonacci_last_digit.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 
-int get_fibonacci_last_digit(int n) {
-  //write your code here
-  return 0;
+int get_fibonacci_last_digit(long long n) {
+    int first = 0;
+    int second = 1;
+
+    int res;
+
+    for (int i = 2; i <= n; i++) {
+        res = (first + second) % 10;
+        first = second;
+        second = res;
+    }
+
+    return res;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  int c = get_fibonacci_last_digit(n);
-  std::cout << c << '\n';
+    int n;
+    std::cin >> n;
+    int c = get_fibonacci_last_digit(n);
+    std::cout << c << '\n';
+
+    return 0;
 }
