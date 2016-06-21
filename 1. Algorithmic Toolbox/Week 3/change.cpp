32de835d179f091ae124e52c3d25c4bb4ae00eb3
aadimator <aadimator@gmail.com>
2016-06-22 01:55:56 +0500

    Problem 1 Complete

--- 1. Algorithmic Toolbox/Week 3/change.cpp
+++ 1. Algorithmic Toolbox/Week 3/change.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
 int get_change(int n) {
-  //write your code here
-  return n;
+    int coins[] = {10, 5, 1};
+    int min = 0;
+
+    for (int i = 0; n > 0; i++) {
+        min += n / coins[i];
+        n %= coins[i];
+    }
+    return min;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  std::cout << get_change(n) << '\n';
+    int n;
+    std::cin >> n;
+    std::cout << get_change(n) << '\n';
 }
