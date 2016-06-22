8118a2a828878c785d8ea994da5ee0a4bb0f9076
aadimator <aadimator@gmail.com>
2016-06-22 15:43:57 +0500

    Problem 4 Complete

--- 1. Algorithmic Toolbox/Week 3/covering_segments.cpp
+++ 1. Algorithmic Toolbox/Week 3/covering_segments.cpp
@@ -1,34 +1,43 @@
 #include <algorithm>
 #include <iostream>
-#include <climits>
-#include <vector>
 
 using std::vector;
 
 struct Segment {
-  int start, end;
+    int start, end;
 };
 
 vector<int> optimal_points(vector<Segment> &segments) {
-  vector<int> points;
-  //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
-  }
-  return points;
+
+    // sort the vector according to the end points
+    std::sort(segments.begin(), segments.end(), [](const Segment &a, const Segment &b) -> bool {
+        return a.end < b.end;
+    });
+
+    vector<int> points; // create a vector to store the common points in the segments
+    int point = segments[0].end; // set the point to the first end point i-e shortest end point
+    points.push_back(point);
+
+    for (size_t i = 1; i < segments.size(); ++i) {
+        if (point < segments[i].start || point > segments[i].end) { // if the point is not in the segment
+            point = segments[i].end; // update the point to the end point of the current segment
+            points.push_back(point); // store it in the vector
+        }
+    }
+
+    return points;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<Segment> segments(n);
-  for (size_t i = 0; i < segments.size(); ++i) {
-    std::cin >> segments[i].start >> segments[i].end;
-  }
-  vector<int> points = optimal_points(segments);
-  std::cout << points.size() << "\n";
-  for (size_t i = 0; i < points.size(); ++i) {
-    std::cout << points[i] << " ";
-  }
+    unsigned int n;
+    std::cin >> n;
+    vector<Segment> segments(n);
+    for (size_t i = 0; i < segments.size(); ++i) {
+        std::cin >> segments[i].start >> segments[i].end;
+    }
+    vector<int> points = optimal_points(segments);
+    std::cout << points.size() << "\n";
+    for (size_t i = 0; i < points.size(); ++i) {
+        std::cout << points[i] << " ";
+    }
 }
