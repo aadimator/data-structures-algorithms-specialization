f28c7077ead42a051e405a9e8d3fdd27559d1144
aadimator <aadimator@gmail.com>
2016-06-22 17:11:28 +0500

    Problem 5 Complete

--- 1. Algorithmic Toolbox/Week 3/different_summands.cpp
+++ 1. Algorithmic Toolbox/Week 3/different_summands.cpp
@@ -4,17 +4,26 @@
 using std::vector;
 
 vector<int> optimal_summands(int n) {
-  vector<int> summands;
-  //write your code here
-  return summands;
+    // using the algorithm described in the pdf
+    vector<int> summands;
+    for (int k = n, l = 1; k > 0; l++) {
+        if (k <= 2 * l) {
+            summands.push_back(k);
+            k -= k;
+        } else {
+            summands.push_back(l);
+            k -= l;
+        }
+    }
+    return summands;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> summands = optimal_summands(n);
-  std::cout << summands.size() << '\n';
-  for (size_t i = 0; i < summands.size(); ++i) {
-    std::cout << summands[i] << ' ';
-  }
+    int n;
+    std::cin >> n;
+    vector<int> summands = optimal_summands(n);
+    std::cout << summands.size() << '\n';
+    for (size_t i = 0; i < summands.size(); ++i) {
+        std::cout << summands[i] << ' ';
+    }
 }
