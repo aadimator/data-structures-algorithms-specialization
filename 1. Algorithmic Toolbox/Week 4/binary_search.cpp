c6c19dbf3a6e7e3eef55f36d05ab23ee4cf06957
aadimator <aadimator@gmail.com>
2016-06-24 19:09:18 +0500

    Problem 1 Complete Binary Search

--- 1. Algorithmic Toolbox/Week 4/binary_search.cpp
+++ 1. Algorithmic Toolbox/Week 4/binary_search.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
-#include <cassert>
 #include <vector>
 
 using std::vector;
 
 int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+    int left = 0, right = (int) a.size();
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        int key = a[mid];
+
+        if (key == x) { return mid; }
+        else if (key > x) { right = mid - 1; }
+        else { left = mid + 1; }
+    }
+
+    return -1;
 }
 
 int linear_search(const vector<int> &a, int x) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
-  }
-  return -1;
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] == x) return i;
+    }
+    return -1;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
-  }
-  int m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (int i = 0; i < m; ++i) {
-    std::cin >> b[i];
-  }
-  for (int i = 0; i < m; ++i) {
-    //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
-  }
+    unsigned int n;
+    std::cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); i++) {
+        std::cin >> a[i];
+    }
+    unsigned int m;
+    std::cin >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; ++i) {
+        std::cin >> b[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        std::cout << binary_search(a, b[i]) << ' ';
+//        std::cout << linear_search(a, b[i]) << ' ';
+//        std::cout << std::endl;
+    }
 }
