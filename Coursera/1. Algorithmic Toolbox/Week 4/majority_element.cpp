563a471687cef8d01ebc2b20ff911c4342d97c41
aadimator <aadimator@gmail.com>
2016-06-28 13:12:26 +0500

    Problem 2 Complete

--- Coursera/1. Algorithmic Toolbox/Week 4/majority_element.cpp
+++ Coursera/1. Algorithmic Toolbox/Week 4/majority_element.cpp
@@ -4,19 +4,37 @@
 
 using std::vector;
 
+int Voting(vector<int> &num) {
+    int count = 0;
+    int candidate = -1;
+
+    for(int i = 0; i < num.size(); ++i) {
+        if(count == 0) {
+            candidate = num[i];
+            count = 1;
+        }
+        else
+            count = (candidate == num[i]) ? ++count : --count;
+    }
+    return candidate;
+}
+
 int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+    int candidate = Voting(a);
+    int count = 0;
+    for (auto i : a) {
+        if (i == candidate) ++count;
+    }
+
+    return (count > (a.size()/2)) ? candidate : -1;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+    int n;
+    std::cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); ++i) {
+        std::cin >> a[i];
+    }
+    std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
 }
