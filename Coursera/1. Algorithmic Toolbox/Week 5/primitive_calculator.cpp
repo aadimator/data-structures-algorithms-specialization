1080c55799dcd46650fa37fe7e4d3e5e7f73f2f8
aadimator <aadimator@gmail.com>
2016-07-14 15:41:34 +0500

    Problem 1 Complete

--- Coursera/1. Algorithmic Toolbox/Week 5/primitive_calculator.cpp
+++ Coursera/1. Algorithmic Toolbox/Week 5/primitive_calculator.cpp
@@ -5,27 +5,35 @@
 using std::vector;
 
 vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
+    std::vector<int> sequence;
+
+    int arr [n + 1] = {0};
+
+    for (int i = 1; i <= n; i++) {
+        arr[i] = arr[i - 1] + 1;
+        if (i % 2 == 0) arr[i] = std::min(1 + arr[i / 2], arr[i]);
+        if (i % 3 == 0) arr[i] = std::min(1 + arr[i / 3], arr[i]);
     }
-  }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
+
+    while (n >= 1) {
+        sequence.push_back(n);
+        if (arr[n - 1] == arr[n] - 1)
+            n = n - 1;
+        else if (n % 2 == 0 && (arr[n / 2] == arr[n] - 1))
+            n = n / 2;
+        else if (n % 3 == 0 && (arr[n / 3] == arr[n] - 1))
+            n = n / 3;
+    }
+    reverse(sequence.begin(), sequence.end());
+    return sequence;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
-    std::cout << sequence[i] << " ";
-  }
+    int n;
+    std::cin >> n;
+    vector<int> sequence = optimal_sequence(n);
+    std::cout << sequence.size() - 1 << std::endl;
+    for (size_t i = 0; i < sequence.size(); ++i) {
+        std::cout << sequence[i] << " ";
+    }
 }
