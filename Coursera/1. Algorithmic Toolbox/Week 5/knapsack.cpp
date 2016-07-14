6df95cdea51e330961728824b7d3ca85dd0f985e
aadimator <aadimator@gmail.com>
2016-07-14 17:57:01 +0500

    Problem 2 Complete

--- Coursera/1. Algorithmic Toolbox/Week 5/knapsack.cpp
+++ Coursera/1. Algorithmic Toolbox/Week 5/knapsack.cpp
@@ -4,22 +4,27 @@
 using std::vector;
 
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+    // Need to understand it later; copied it from another solution posted online
+    vector<vector<int>> values(w.size() + 1, vector<int>((unsigned int) (W + 1)));
+
+    for (int i = 1; i < values.size(); i++) {
+        for (int j = 1; j < values[0].size(); j++) {
+            values[i][j] =
+                    w[i - 1] > j
+                    ? values[i - 1][j]
+                    : std::max(w[i - 1] + values[i - 1][j - w[i - 1]], values[i - 1][j]);
+        }
     }
-  }
-  return current_weight;
+    return values[w.size()][W];
 }
 
 int main() {
-  int n, W;
-  std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
-  std::cout << optimal_weight(W, w) << '\n';
+    int W;
+    unsigned int n;
+    std::cin >> W >> n;
+    vector<int> w(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> w[i];
+    }
+    std::cout << optimal_weight(W, w) << '\n';
 }
