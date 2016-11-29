d4935b49dc1ee67b09b5fe1e3dec8b49ec5fa6a8
aadimator <aadimator@gmail.com>
2016-11-29 21:02:33 +0500

    DS:3 - hash_substring complete

--- 2. Data Structures/Week 3/hash_substring.cpp
+++ 2. Data Structures/Week 3/hash_substring.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 using std::string;
-typedef unsigned long long ull;
+typedef long long ll;
 
 struct Data {
     string pattern, text;
@@ -15,18 +15,54 @@ Data read_input() {
     return data;
 }
 
-void print_occurrences(const std::vector<int>& output) {
+void print_occurrences(const std::vector<ll> &output) {
     for (size_t i = 0; i < output.size(); ++i)
         std::cout << output[i] << " ";
     std::cout << "\n";
 }
 
-std::vector<int> get_occurrences(const Data& input) {
-    const string& s = input.pattern, t = input.text;
-    std::vector<int> ans;
-    for (size_t i = 0; i + s.size() <= t.size(); ++i)
-        if (t.substr(i, s.size()) == s)
-            ans.push_back(i);
+ll PolyHash(const string &s, ll p, ll x) {
+    ll hash = 0;
+    for (int i = static_cast<int> (s.size()) - 1; i >= 0; --i)
+        hash = (hash * x + s[i]) % p;
+    return hash;
+}
+
+std::vector<long long> PreComputeHashes(const string &T, ll P_length, ll p, ll x) {
+    ll T_P_length = (ll) (T.length() - P_length);
+    std::vector<long long> H((unsigned long) (T_P_length + 1));
+    string S = T.substr((unsigned long) T_P_length, (unsigned long) P_length);
+    H[T_P_length] = PolyHash(S, p, x);
+
+    ll y = 1;
+    for (ll i = 1; i <= P_length; i++) {
+        y = ((y * x) % p + p) % p;
+    }
+    for (long long i = T_P_length - 1; i >= 0; i--) {
+        H[i] = ((x * H[i + 1] + T[i] - y * T[i + P_length]) % p + p) % p;
+    }
+    return H;
+}
+
+std::vector<ll> RabinKarp(const string &T, const string &P) {
+    ll p = 500009;
+    ll x = 139;
+    std::vector<ll> result;
+    ll pHash = PolyHash(P, p, x);
+    std::vector<long long> H = PreComputeHashes(T, (ll) P.length(), p, x);
+
+    for (ll i = 0; i <= T.length() - P.length(); i++) {
+        if (pHash == H[i] && T.substr((unsigned long) i, P.length()) == P) {
+            result.push_back(i);
+        }
+    }
+    return result;
+}
+
+std::vector<ll> get_occurrences(const Data &input) {
+    const string &s = input.pattern, t = input.text;
+    std::vector<ll> ans;
+    ans = RabinKarp(t, s);
     return ans;
 }
 
